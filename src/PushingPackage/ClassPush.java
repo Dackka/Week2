98aac8048b59f0eb72c2303aac196a233fa07f57
Dackka <stevengeorgejamescarleton@gmail.com>
2019-05-16 10:01:46 -0400

    This took to much effort, figuring out how to push things.

---
+++ src/PushingPackage/ClassPush.java
@@ -0,0 +1,15 @@
+/*
+ * Name: Steven Carleton
+ * Assignment:
+ * Program: Computer Programmer
+ *
+ *
+ */
+package PushingPackage;
+/**
+ * @author Steven Carleton
+ */
+public class ClassPush
+{
+//this class is going to be pushed to github
+}
